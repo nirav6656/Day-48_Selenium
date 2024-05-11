3611098824a43be09476f3acff10207be9bc2889
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-05-11 11:53:10 -0400

    fill out form using selenium.

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
+# Keep Chrome browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
+
+# Create and configure the Chrome webdriver
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6")
-username = driver.find_element(By.XPATH, '//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[3]/span[2]')
-print(username)
-driver.close()
+# Navigate to the (fake) newsletter registration page
+driver.get("https://secure-retreat-92358.herokuapp.com/")
+
+# Find the first name, last name, and email fields
+first_name = driver.find_element(By.NAME, value="fName")
+last_name = driver.find_element(By.NAME, value="lName")
+email = driver.find_element(By.NAME, value="email")
+
+# Fill out the form
+first_name.send_keys("Angela")
+last_name.send_keys("Yu")
+email.send_keys("angela@email.com")
+
+# Locate the "Sign Up" button. Then click on it
+submit = driver.find_element(By.CSS_SELECTOR, value="form button")
+submit.click()
