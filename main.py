baa2de15c069abfa9868833b6407a837136243af
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-05-13 22:01:34 -0400

    complete the project with auto-purchase functionality and autoclick to create more cookies.

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+import time
 
-# Keep Chrome browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-# Create and configure the Chrome webdriver
 driver = webdriver.Chrome(options=chrome_options)
 
-# Navigate to the (fake) newsletter registration page
-driver.get("https://secure-retreat-92358.herokuapp.com/")
+driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
-# Find the first name, last name, and email fields
-first_name = driver.find_element(By.NAME, value="fName")
-last_name = driver.find_element(By.NAME, value="lName")
-email = driver.find_element(By.NAME, value="email")
+first_name = driver.find_element(By.ID, "cookie")
+for i in range(50):
+    cookie = driver.find_element(By.ID, "cookie")
+    cookie.click()
+    time.sleep(1)
+money = int(driver.find_element(By.ID,"money").text)
+def cookie():
+    for i in reversed(range(0,8)):
+        count = int(driver.find_elements(By.CSS_SELECTOR,"#store b")[i].text.split(" ")[-1].replace(",",""))
+        if money > count:
+            driver.find_element(By.CSS_SELECTOR,"#store b").click()
 
-# Fill out the form
-first_name.send_keys("Angela")
-last_name.send_keys("Yu")
-email.send_keys("angela@email.com")
 
-# Locate the "Sign Up" button. Then click on it
-submit = driver.find_element(By.CSS_SELECTOR, value="form button")
-submit.click()
+timeout = time.time() + 60*5    # 5 minutes from now
+while True:
+    test = 0
+    cookie()
+    if test == 5 or time.time() > timeout:
+        break
+    test = test - 1
+
+driver.quit()
